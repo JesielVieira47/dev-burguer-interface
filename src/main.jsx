2642ad90a449a1da474f98a62fd1ba9ea09f33b8
JesielVieira47 <jesiel_vig@hotmail.com>
2026-08-18 17:27:11 -0300

    fix: adiciona basename e ajustes do gh-pages

--- src/main.jsx
+++ src/main.jsx
@@ -15,15 +15,15 @@ import { Router } from './routes';
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider theme={standardTheme}>
-    <AppProvider>
-      <Elements stripe={stripePromise}>
-        <BrowserRouter>
-        <Router />
-        </BrowserRouter>
-      </Elements>
-      <GlobalStyles />
-      <ToastContainer autoClose={2000} theme="colored" />
-    </AppProvider>
+      <AppProvider>
+        <Elements stripe={stripePromise}>
+          <BrowserRouter basename={import.meta.env.BASE_URL}>
+            <Router />
+          </BrowserRouter>
+        </Elements>
+        <GlobalStyles />
+        <ToastContainer autoClose={2000} theme="colored" />
+      </AppProvider>
     </ThemeProvider>
   </StrictMode>,
 );
